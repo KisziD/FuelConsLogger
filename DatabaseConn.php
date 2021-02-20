1ce6f8ab64eb002159768a4fdb35e9e544a4b7c7
Kiszi <kiszidomi1@gmail.com>
2021-02-20 18:53:38 +0100

    database

--- DatabaseConn.php
+++ DatabaseConn.php
@@ -4,7 +4,19 @@
     {
         public function Connect()
         {
-           //conn
+            $User = 'epiz_27977918';
+            $Pass = 'LNowMo1V8zp9fih';
+            $Database = 'epiz_27977918_MainDB';
+            $Server = 'sql111.epizy.com';
+        
+            $conn = new mysqli($Server, $User, $Pass, $Database);
+        
+            if ($conn->connect_error) {
+                echo "hiba";
+                die("connection failed: ".$conn->connect_error);
+            }else{
+                echo "done";
+            }
         }
     }
 ?>
