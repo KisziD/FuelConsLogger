10fb49a6a92489e9aa802730b160b3bb8d7b68ad
Kiszi <kiszidomi1@gmail.com>
2021-02-20 18:23:12 +0100

    teszt

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
+	
+	include $_SERVER['DOCUMENT_ROOT'] . "/DatabaseConn.php";
+	$con = new DatabaseManager();
+	echo $con->Connect();
+
 	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
 		$uri = 'https://';
 	} else {
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/Main.html');
+	//header('Location: '.$uri.'/Main.html');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
