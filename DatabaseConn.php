04add47dbc67a6f140ac381ec0e628f261439070
Kiszi <kiszidomi1@gmail.com>
2021-02-20 18:29:47 +0100

    raul

--- DatabaseConn.php
+++ DatabaseConn.php
@@ -4,15 +4,7 @@
     {
         public function Connect()
         {
-            // PHP Data Objects(PDO) Sample Code:
-            try {
-                $conn = new PDO("sqlsrv:server = tcp:projmain.database.windows.net,1433; Database = MainDB", "kiszi", "ProjectMainLoginPass21");
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-            catch (PDOException $e) {
-                print("Error connecting to SQL Server.");
-                die(print_r($e));
-            }
+           
         }
     }
 ?>
