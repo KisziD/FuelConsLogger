d474ce24d42927b5d67a26bee91b8643f7bf7ae1
Kiszi <kiszidomi1@gmail.com>
2021-03-01 15:46:25 +0100

    Design

--- Javascript/Register.js
+++ Javascript/Register.js
@@ -1,10 +1,32 @@
-function validate() {
+
+function nameValidate() {
+    var nameInput = document.getElementById("name");
+    var name = nameInput.value;
+
+    if(name!=""){
+        nameInput.classList.remove("is-invalid");
+        nameInput.classList.add("is-valid");
+    }else{
+        nameInput.classList.remove("is-valid");
+        nameInput.classList.add("is-invalid");
+    }
+}
+
+function passwordValidate() {    
+}
+
+function usernameValidate() {
+}
+
+function emailValidate() {
+}
+
+/*function validate() {
 
 
     var usernameInput = document.getElementById("username");
     var username = usernameInput.value;
-    var nameInput = document.getElementById("name");
-    var name = nameInput.value;
+   
     var emailInput = document.getElementById("email");
     var email = emailInput.value;
     var passwordInput = document.getElementById("password");
@@ -13,6 +35,5 @@ function validate() {
     if(username!="" && username.split(" ").length < 2){
         console.log(username);
     }
-
     
-}
+}*/
